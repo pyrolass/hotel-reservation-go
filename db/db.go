2527f42cb188f5889187cc4d17847f3936e1f441
pyrolass <las.suleman1@gmail.com>
2024-03-24 21:54:51 +0300

    go xosha

--- db/db.go
+++ db/db.go
@@ -1,15 +1,3 @@
 package db
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
-
 const DBNAME = "hotel-reservation"
-
-func ToObjectId(id string) primitive.ObjectID {
-	objectId, err := primitive.ObjectIDFromHex(id)
-
-	if err != nil {
-		panic(err)
-	}
-
-	return objectId
-}
--- db/user_store.go
+++ db/user_store.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/pyrolass/hotel-reservation-go/entities"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -29,8 +30,17 @@ func NewMongoUserStore(client *mongo.Client) *MongoUserStore {
 }
 
 func (s *MongoUserStore) GetUserById(ctx context.Context, id string) (*entities.User, error) {
+
+	// validation of id
+
+	oid, err := primitive.ObjectIDFromHex(id)
+
+	if err != nil {
+		return nil, err
+	}
+
 	var user entities.User
-	if err := s.coll.FindOne(ctx, bson.M{"_id": ToObjectId(id)}).Decode(&user); err != nil {
+	if err := s.coll.FindOne(ctx, bson.M{"_id": oid}).Decode(&user); err != nil {
 		return nil, err
 	}
 
