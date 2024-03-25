620c0f15d5a1f053a093e02b4b371411f8c7926e
pyrolass <las.suleman1@gmail.com>
2024-03-25 22:36:21 +0300

    changes

---
+++ entities/hotel.go
@@ -0,0 +1,28 @@
+package entities
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+type Hotel struct {
+	ID       primitive.ObjectID   `bson:"_id,omitempty" json:"id,omitempty"`
+	Name     string               `bson:"name" json:"name"`
+	Location string               `bson:"location" json:"location"`
+	Rooms    []primitive.ObjectID `bson:"rooms" json:"rooms"`
+}
+
+type Room struct {
+	ID        primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
+	Type      RoomType           `bson:"type" json:"type"`
+	BasePrice float64            `bson:"basePrice" json:"basePrice"`
+	Price     float64            `bson:"price" json:"price"`
+	Occupied  bool               `bson:"occupied" json:"occupied"`
+	HotelID   primitive.ObjectID `bson:"hotelID" json:"hotelID"`
+}
+
+type RoomType int
+
+const (
+	_ RoomType = iota
+	SinglePerson
+	DoublePerson
+	TriplePerson
+)
